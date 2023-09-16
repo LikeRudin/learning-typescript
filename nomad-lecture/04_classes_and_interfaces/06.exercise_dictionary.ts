d46c380864b6600b920f4bd9bf4c724ae1c58a3b
Young Jin <118636461+LikeRudin@users.noreply.github.com>
2023-09-16 14:28:55 +0900

    add detail ideas

--- nomad-lecture/04_classes_and_interfaces/06.exercise_dictionary.ts
+++ nomad-lecture/04_classes_and_interfaces/06.exercise_dictionary.ts
@@ -122,9 +122,56 @@
 // myDict.bulkDelete(["김치", "아파트"]);
 // myDict.showAll();
 
+// 관점포인트
+
+// 1. TSDoc 스타일의 주석을 작성했습니다.
+// TSDoc은  JSDoc의 TypeScript 스타일 작성법입니다.
+// 기존 JSDoc에서 method param 타입이나 arg에 관한 설명을 제거하고
+// 코드의 동작 및 역할만 기술한  문서를 작성한 것입니다.
+// dictionary class 에서 method identifier에 마우스 커서를 올리면 
+// /** */ 내부에 기입한 설명을 볼 수 있습니다.
+
+// 2. Interface를 사용하여 dictionary Class의 구성 요소를 선언했습니다.
+//  class가 interface에게서 상속받은 모든 속성은 public이 됩니다.
+//  단어를 저장하는 역할의 객체는 private이 되어야 하므로,
+//  interface 내부가아니라 dictionary class 내부에서 만들었습니다.
+
+// 3. add method의 입력 데이터가 Word class의 instance라고 가정했습니다.
+//  dictionary add method의 인자 타입 검증 책임을 Word class 에게 넘겨주므로
+//  사전에 넣을 단어를 만들때, "추가적 타입 검증없이" 왕창 뽑아내기 편해집니다.
+//  이는 테스트 코드에서 확인하실수있습니다.
+
+// 4. const enum을 사용했습니다.
+// enum은 자바스크립트에 없는 구성요소입니다.
+// 만약 그냥 enum 키워드만으로 선언하면 자바스크립트로 컴파일되어
+// 코드에 남게되고, 빌드해도 프로덕션코드에서 사라지지않습니다.
+// 꼭 const를 붙여주세요.
+// 이 사실은 오른쪽의 JS 탭에서 컴파일된 코드를 통해 확인할 수있습니다.
+
+// 5. 각 method별 존재 의의에대해 고민했습니다.
+// update와 add, 그리고 upsert는 모두 다른 내부 코드를 가집니다.
+// add는 해당 단어가 없을경우에만, 추가합니다.
+// update는 해당 단어가 있을경우에만, 업데이트합니다.
+
+// 6. 각 method의 반환값을 boolean으로 하여 실행의 성공여부를 알려줍니다.
+// update의 책임은 단어를 추가하는것이아니라 이미있는 단어의 정의를 수정하는것입니다.
+//  실행했을때, 사전에 해당 단어가없으면 false,  단어가있다면 true 를반환하게 했습니다.
+// add, delete 등의 metohd 에도 같은방식으로 적용해주었습니다.
+// bulk method는 각 각의 단어에대한 결과를 저장한 boolean[] 배열을 반환합니다.
+
+// 7. class 내부에서 각 method를 재활용했습니다.
+// add, update의 동작은 exists를 통해 결정합니다.
+// bulkAdd는 add를 여러번 호출하는식으로 작동합니다
+
+// 8. 아쉬운점: upsert를 실행할때 exists가 두번실행됩니다.
+
+
+
+
+// 타입 구현부 
 
 /**
- * object: word를 표현하는 object
+ * 사전에 등재할 단어
  */
 type WordObject = {
     term:string,
@@ -132,16 +179,10 @@ type WordObject = {
 }
 
 /**
- * class: 사전에 추가할 word 하나를 표현 
+ * 사전에 등재할 단어
  */
 class Word {
-    /**
-     * word의 단어
-     */
     term:string;
-    /**
-     * word의 정의
-     */
     definition:string;
     constructor(term:string, definition:string) {
         this.term = term;
@@ -150,64 +191,58 @@ class Word {
 }
 
 /**
- * enum: upsert가 수행한 함수
- *
+ * upsert가 수행한 함수의 종류
 */
 const enum UpsertReturn {
-    /**
-     * update 수행시 return 값
-     */
     Update = "update",
-    /**
-     * add 수행시 return 값
-     */
     Add = "add"
 }
 
 /**
- * Dictionary의 메서드를 포함한 interface
- * @주의: interface를 상속받은경우, private modifier를 붙여줄 수없습니다.
+ * 단어의 추가, 검색, 삭제, 업데이트, 수정, 등재확인 등을 할 수있는 사전
  */
 interface IDictionary{
     /**
-     * method: 단어를 사전에 추가
+     * 사전에 단어를 추가합니다.
      */
     add: (word:Word) => boolean;
     /**
-     * method: 단어를 사전에서 검색
+     * 사전에서 단어를 검색합니다.
      */
     get: (term:string) => string | undefined;
     /**
-     * method: 단어를 사전에서 삭제
+     * 단어를 사전에서 삭제합니다.
      */
     delete: (term:string) => boolean;
     /**
-     * method: 존재하는 단어의 정의를 업데이트
+     * 사전에 등재되어있는 단어의 정의를 업데이트합니다.
      */
     update: (word:Word) => boolean;
     /**
-     * method: 존재하는 단어 전부 출력
+     * 사전에 등재되어있는 모든 단어를 콘솔에 출력합니다.
      */
     showAll: () => void;
     /**
-     * method: 사전에 저장된 단어 수 반환
+     * 사전에 등재되어있는 단어의 수를 반환합니다.
      */
     count: () => number;
     /**
-     * method: 단어를 업데이트 하거나/ 추가
+     * 사전에 단어가 등재되어있는지 확인합니다.
+     * 단어가 존재하지 않으면 단어를 추가합니다.
+     * 단어가 존재한다면 정의를 업데이트합니다.
      */
     upsert: (word:Word) => UpsertReturn;
-    /**
-     * method: 단어가 사전에 존재하는지 확인
-     */
+    /** 
+     * 단어가 사전에 등재되어있는지 확인합니다.
+     *  */
     exists: (term: string) => boolean;
-    /**
-     * method: 단어를 한꺼번에 사전에 추가
-     */
+    /** 
+     * 사전에 여러 단어를 한번에 추가합니다.
+     *  */
     bulkAdd: (wordObjects: WordObject[]) => boolean[];
     /**
-     * method: 단어를 한꺼번에 사전에서 제거
-     */
+     * 여러 단어를 사전에서 한번에 삭제합니다.
+    */
     bulkdelete: (terms: string[]) => boolean[];
 }
 
@@ -288,6 +323,8 @@ class Dictionary implements IDictionary{
     }
 
 }
+
+// 테스트 코드
 
 const test = () => {
     console.log("test start");
